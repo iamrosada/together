c3438318ee0298088892b78eecd3551b51960e49
77-aguarosada <luisrosada2018@gmail.com>
2021-06-26 19:12:53 +0300

    created the first route

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,7 +1,12 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
+
+app.get('/test',(request, response)=>{
+ return response.send("ola");
+});
+
 app.listen(3333, ()=>{
-    console.log('server is running')
+    console.log('server is running');
 })
