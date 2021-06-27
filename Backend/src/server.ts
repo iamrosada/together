eefa042a5a920eb1f9d8a1f02dfb87751b0313a6
77-aguarosada <luisrosada2018@gmail.com>
2021-06-27 12:29:22 +0300

    updated serverts

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 
 import { router } from "../src/routes";
@@ -13,17 +13,19 @@ app.use(express.json());
 app.use(router);
 
 //para ler os erros
-app.use((err: Error, request: Request, response: Response) => {
-  if (err instanceof Error) {
-    return response.status(400).json({
-      error: err.message,
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
     });
   }
-  return response.status(500).json({
-    status: "error",
-    message: "Internal Server Error",
-  });
-});
+);
 app.listen(3333, () => {
   console.log("server is running");
 });
