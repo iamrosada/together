fb560bb6a6ec51337122979d699b985aaf5a5085
77-aguarosada <luisrosada2018@gmail.com>
2021-06-27 08:14:09 +0300

    first updated

--- Backend/src/entities/User.ts
+++ Backend/src/entities/User.ts
@@ -1,6 +1,18 @@
+import { Entity } from "typeorm";
+
+@Entity('users')
 class User{
+  id: string ;
+  
+  name: string;
 
+  email: string;
 
+  admin: string;
 
+  created_at: Date;
+
+  updated_at: Date;
+  
 }
 export {User}
