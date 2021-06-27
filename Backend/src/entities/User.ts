80c98065600245aab26b636638049b5ad4082227
77-aguarosada <luisrosada2018@gmail.com>
2021-06-27 10:08:39 +0300

    new updated with prettier

--- Backend/src/entities/User.ts
+++ Backend/src/entities/User.ts
@@ -1,18 +1,36 @@
-import { Entity } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
 
-@Entity('users')
-class User{
-  id: string ;
-  
+@Entity("users")
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
   name: string;
 
+  @Column()
   email: string;
 
-  admin: string;
+  @Column()
+  admin: boolean;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
-  
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
 }
-export {User}
+export { User };
