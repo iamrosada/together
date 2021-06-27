9096655855a51de703e45c010bbca13877d38305
77-aguarosada <luisrosada2018@gmail.com>
2021-06-27 12:09:20 +0300

    added function for error

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { router } from "../src/routes";
 
 const app = express();
@@ -9,6 +9,18 @@ app.use(express.json());
 
 app.use(router);
 
+//para ler os erros
+app.use((err: Error, request: Request, response: Response) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      error: err.message,
+    });
+  }
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
 app.listen(3333, () => {
   console.log("server is running");
 });
